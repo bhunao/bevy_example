8f864cdf59fa4f31aab1cf3e4ae85cc3192b3cd5
bhunao <bhunao@gmail.com>
2023-04-11 19:54:16 -0300

    finish 2

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use bevy::{prelude::*, window::PrimaryWindow};
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)          // all default plugins by bevy
-        .add_startup_system(spawn_player)
-        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_player)           // spawn player once at start
+        .add_startup_system(spawn_camera)           // spawn camera once at start
         .run();
 }
 
@@ -17,12 +17,12 @@ pub fn spawn_player(
     window_query: Query<&Window, With<PrimaryWindow>>,
     asset_server: Res<AssetServer>,
 ) {
-    let window = window_query.get_single().unwrap();
+    let window = window_query.get_single().unwrap();    // get the window
 
-    commands.spawn((
+    commands.spawn((        // `SpriteBundle` and `Player` structs inside the bundle
             SpriteBundle {
                 transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0), // spawn in center
-                texture: asset_server.load("sprites\\ball_blue_large.png"),
+                texture: asset_server.load("sprites\\ball_blue_large.png"),     // load sprite image
                 ..default()
             },
             Player {},
@@ -33,11 +33,11 @@ pub fn spawn_camera(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
-    let window = window_query.get_single().unwrap();
+    let window = window_query.get_single().unwrap();    // get the window
 
-    commands.spawn(
+    commands.spawn(     // bundle with onle the camera
         Camera2dBundle {
-            transform: Transform::from_xyz(window.width() / 2.0 , window.height() / 2.0, 0.0),
+            transform: Transform::from_xyz(window.width() / 2.0 , window.height() / 2.0, 0.0),      // spawn in center
             ..default()
         }
     );
