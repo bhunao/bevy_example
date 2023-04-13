db2c4045e98bc9ab2d397ae607f0d6f12c27a36c
bhunao <bhunao@gmail.com>
2023-04-12 21:59:35 -0300

    sad feelings

--- src/02.setup_bundle_resources.rs
+++ src/02.setup_bundle_resources.rs
@@ -42,32 +42,3 @@ pub fn spawn_camera(
         }
     );
 }
-
-pub fn player_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<&mut Transform, With<Player>>,
-    time: Res<Time>
-) {
-    if let Ok(mut transform) = player_query.get_single_mut() {
-        let mut direction = Vec::ZERO;
-
-        if keyboard_input.pressed(KeyCode::A) {
-            direction += Vec3::new(-1.0, 0.0, 0.0);
-        }
-        if keyboard_input.pressed(KeyCode::D) {
-            direction += Vec3::new(1.0, 0.0, 0.0);
-        }
-        if keyboard_input.pressed(KeyCode::W) {
-            direction += Vec3::new(0.0, 1.0, 0.0);
-        }
-        if keyboard_input.pressed(KeyCode::S) {
-            direction += Vec3::new(0.0, -1.0, 0.0);
-        }
-
-        if direction.lenght() > 0.0 {
-            direction = direction.normalize();
-        }
-
-        transform.translation += direction * PLAYER_SPEED * time.detal_seconds();
-    };
-}
